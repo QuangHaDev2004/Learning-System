727c6ef894c135dfcb57c40d880cb42c39eb4f4d
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-07-03 15:02:48 +0700

    redeploy webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import createUser from "@/lib/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 export async function POST(req: Request) {
@@ -30,7 +32,20 @@ export async function POST(req: Request) {
 
   const eventType = msg.type;
   if (eventType == "user.created") {
-    console.log(msg.data);
+    const { id, username, email_addresses, image_url } = msg.data;
+    const user = await createUser({
+      clerkId: id,
+      username: username!,
+      name: username!,
+      email: email_addresses[0].email_address,
+      avatar: image_url
+    });
+
+    return NextResponse.json({
+      message: "OK",
+      user
+    })
+
   }
 
   // Rest
